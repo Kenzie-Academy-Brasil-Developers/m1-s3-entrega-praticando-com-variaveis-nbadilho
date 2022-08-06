3f8d399439eee1387a5ebd096cd96c62ccd51cac
Natalia <nataliabadilho@hotmail.com>
2022-08-06 19:33:08 -0300

    entrega 1

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome="Natalia" ;
+
+let sobrenome="Badilho" ; 
+
+let anoNascimento=2003; 
+
+let idade=2022-2003;
+
+let cidade="São Paulo" ;
+
+console.log(`Eu sou a ${nome}, tenho ${idade} e moro em ${cidade}.`)
